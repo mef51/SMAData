df35375d2446799582bfe5fdb9401446f1d3f916
mef51 <hms.mohammed@gmail.com>
2017-09-21 17:42:02 -0400

    HH211: a more recent observation has data

---
+++ 130121_HH211_Lee/plot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import miriad
+
+def plotPhase(vis, device='2/xs'):
+	miriad.smauvplt({
+		'vis': vis,
+		'device': device,
+		'interval': '1e3',
+		'stokes': 'i,v',
+		'axis': 'time,pha',
+		'nxy': '2,2'
+	})
+
+vis = 'UVDATA/combined.usb.stokes'
+
+miriad.uvspec({
+	'vis': vis,
+	'device': '1/xs',
+	'interval': 9999,
+	'options': 'avall,nobase',
+	'nxy': '1,1',
+	'stokes': 'i,v',
+	'axis': 'freq,amp',
+	# 'line': miriad.averageVelocityLine(vis, factor=1),
+})
+

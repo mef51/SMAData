f629bbde9388388888347b503d1f40fac5e4c459
mef51 <hms.mohammed@gmail.com>
2017-10-18 21:02:07 -0400

    fix resolution problem for full-band irc+10216 map

--- IRC+10216/Correct_RR_LL_offset.py
+++ IRC+10216/Correct_RR_LL_offset.py
@@ -10,22 +10,23 @@ if __name__ == '__main__':
 	mapdir = 'MAPScorrect'
 
 	lines = ['cs7-6', 'sis19-18', 'h13cn4-3', 'co3-2', 'cnt.usb', 'usb']
-	input("Press return to split")
-	squint.split(uvo, uvc, so, lines)
-	input("Press return to selfcal")
-	squint.selfcal(so, uvc, lines)
-	input("Press return to map visibilities")
-	squint.mapvis(uvo, uvc, so, mapdir, lines[:],
-		lineSelection=[None, None, None, None]
-	)
+	# input("Press return to split")
+	# squint.split(uvo, uvc, so, lines)
+	# input("Press return to selfcal")
+	# squint.selfcal(so, uvc, lines)
+	# input("Press return to map visibilities")
+	# squint.mapvis(uvo, uvc, so, mapdir, lines[:],
+	# 	lineSelection=[None, None, None, None]
+	# )
 	input("Press return to map visibilities with frequency axis")
-	lsel = 'vel,8,-40,4,4'
+	lsels = ['vel,8,-40,4,4' for l in lines]
+	lsels[lines.index('usb')] = miriad.averageVelocityLine('UVOffsetCorrect/irc+10216.usb.corrected.slfc', 17)
 	squint.mapallvis(uvo, uvc, so, mapdir, lines[:],
-		lineSelection=[lsel for l in lines]
+		lineSelection=lsels
 	)
-	input("Press return to save plots")
-	vrms = 0.13
-	squint.disp(uvo, uvc, so, mapdir,
-		lines=['cs7-6', 'sis19-18', 'h13cn4-3', 'co3-2', 'cnt'],
-		stokesVrms=[vrms for l in lines]
-	)
+	# input("Press return to save plots")
+	# vrms = 0.13
+	# squint.disp(uvo, uvc, so, mapdir,
+	# 	lines=['cs7-6', 'sis19-18', 'h13cn4-3', 'co3-2', 'cnt'],
+	# 	stokesVrms=[vrms for l in lines]
+	# )

717762dd5090c58f485afbb234e1e40c875242f4
mef51 <hms.mohammed@gmail.com>
2018-05-08 17:30:45 -0400

    fix maps. was using wrong data on some of them. add beam map.

--- squintscripts/cleandemo.py
+++ squintscripts/cleandemo.py
@@ -24,14 +24,19 @@ cgdispOptions = {
 }
 miriad.cgdisp(cgdispOptions)
 
-cgdispOptions['in'] = '../{0}/MAPSCorrect/{1}.cnt.i.{2},../{0}/MAPSCorrect/{1}.co3-2.i.{2}'.format(folders[0], sources[0], maps[1])
+cgdispOptions['in'] = '../{0}/MAPSCorrect/{1}.co3-2.i.{2},../{0}/MAPSCorrect/{1}.co3-2.i.{2}'.format(folders[0], sources[0], maps[1])
 cgdispOptions['device'] = 'irc+10216.co3-2.demo.{}.ps/cps'.format(maps[1])
 cgdispOptions['olay'] = 'irc+10216.{}.olay'.format(maps[1])
 # cgdispOptions['slev'] = 'p,1,a,{}'.format(0.02)
 miriad.cgdisp(cgdispOptions)
 
-cgdispOptions['in'] = '../{0}/MAPSCorrect/{1}.cnt.i.{2},../{0}/MAPSCorrect/{1}.co3-2.i.{2}'.format(folders[0], sources[0], maps[2])
+cgdispOptions['in'] = '../{0}/MAPSCorrect/{1}.co3-2.i.{2},../{0}/MAPSCorrect/{1}.co3-2.i.{2}'.format(folders[0], sources[0], maps[2])
 cgdispOptions['device'] = 'irc+10216.co3-2.demo.{}.ps/cps'.format(maps[2])
 cgdispOptions['olay'] = 'irc+10216.{}.olay'.format(maps[2])
 # cgdispOptions['slev'] = 'p,1,a,{}'.format(0.02)
+miriad.cgdisp(cgdispOptions)
+
+cgdispOptions['in'] = '../{0}/MAPSCorrect/{1}.co3-2.bm,../{0}/MAPSCorrect/{1}.co3-2.bm'.format(folders[0], sources[0], maps[2])
+cgdispOptions['device'] = 'irc+10216.co3-2.demo.bm.ps/cps'
+cgdispOptions['olay'] = 'irc+10216.bm.olay'.format(maps[2])
 miriad.cgdisp(cgdispOptions)

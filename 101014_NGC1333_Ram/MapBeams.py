4ebc1b9daf02db7f8e2431ccb28359c075dd30be
mef51 <hms.mohammed@gmail.com>
2018-06-14 17:10:35 -0400

    investigate beams of different polarizations

---
+++ 101014_NGC1333_Ram/MapBeams.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import shutil, glob, os
+import miriad
+
+so = 'iras2a.aver'
+line = 'vel,18,-43,2,2'
+rms = 0.3
+
+molecule = 'CO'
+if molecule == 'CO':
+	freq = 345.7959
+	lab = 'co3-2'
+	reg = 'arcsec,box(-4,-4,4,4)'
+	rms = 0.15
+	rng = '0,30'
+
+src = 'MAPSCorrect/{}.{}'.format(so, lab)
+
+print('LL BEAM')
+vis = 'UVOffsetCorrect/{}.{}.ll'.format(so, lab)
+miriad.invert({
+	'vis': vis,
+	'line': line,
+	'beam': '{}.ll.bm'.format(src),
+	'map': '{0}.ll.mp'.format(src),
+	'imsize': 128,
+	'cell': 0.3,
+	'options': 'systemp,double',
+	# 'stokes': 'i,q,u,v',
+	'sup': 0
+})
+
+print('RR BEAM')
+vis = 'UVOffsetCorrect/{}.{}.rr'.format(so, lab)
+miriad.invert({
+	'vis': vis,
+	'line': line,
+	'beam': '{}.rr.bm'.format(src),
+	'map': '{0}.rr.mp'.format(src),
+	'imsize': 128,
+	'cell': 0.3,
+	'options': 'systemp,double',
+	# 'stokes': 'i,q,u,v',
+	'sup': 0
+})
